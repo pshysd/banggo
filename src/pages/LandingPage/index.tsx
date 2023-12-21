f2a64a1303069e6425365e6925aab40e43be3bbf
PSH <pshysd@kakao.com>
2023-12-21 16:07:58 +0900

    [ FEAT ] 랜딩 페이지 완성

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import Typed from 'typed.js';
 
 function LandingPage() {
+	const el = useRef(null);
+
 	const navigate = useNavigate();
 
 	const onClickSignup = () => {
@@ -13,6 +16,17 @@ function LandingPage() {
 		navigate('/login');
 	};
 
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: ['HELLO!'],
+			typeSpeed: 70,
+			backSpeed: 30,
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
 	return (
 		<div style={{ display: 'flex', width: '100dvw', height: '100dvh' }}>
 			<div
@@ -22,24 +36,33 @@ function LandingPage() {
 					backgroundColor: '#38419D',
 				}}
 			>
-				12345
+				<h1
+					style={{
+						color: '#ffffff',
+						paddingLeft: '3%',
+						paddingTop: '2%',
+						fontSize: '1.2rem',
+					}}
+				>
+					BANGGO
+				</h1>
+				<p ref={el} style={{ paddingTop: '35dvh', paddingLeft: '5dvh', color: '#ffffff', fontSize: '3rem' }} />
 			</div>
 			<div
 				style={{
 					display: 'flex',
 					justifyContent: 'center',
 					alignItems: 'center',
+					flexDirection: 'column',
 					width: '40dvw',
 					height: '100dvh',
 					backgroundColor: '#200E3A',
-					paddingTop: '20vh',
-					paddingBottom: '20vh',
 				}}
 			>
-				<Button variant="outline-info" style={{width: '50%'}} onClick={onClickLogin}>
+				<Button variant="outline-info" style={{ width: '60%' }} onClick={onClickLogin}>
 					로그인
 				</Button>
-				<Button variant="outline-info" onClick={onClickSignup}>
+				<Button variant="outline-info" style={{ width: '60%', marginTop: '0.5%' }} onClick={onClickSignup}>
 					회원가입
 				</Button>
 			</div>
