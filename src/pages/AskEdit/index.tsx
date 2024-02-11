cee0063fefd5f7892d19d159ad65c0b4de451305
PSH <pshysd@kakao.com>
2024-02-11 14:43:45 +0900

    DELETE

--- src/pages/AskEdit/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AskEdit() {
-	return <div>AskEdit</div>;
-}
-
-export default AskEdit;
--- src/pages/MainPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Drawer from '@components/Drawer';
-
-function MainPage() {
-	return <Drawer />;
-}
-
-export default MainPage;
