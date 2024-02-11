9d1d91e581c298a937738f9ece41596a9f6c3d3c
PSH <pshysd@kakao.com>
2024-02-11 14:33:28 +0900

    MODIFY: 중첩 라우팅 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import loadable from '@loadable/component';
-import { Route, Routes } from 'react-router-dom';
-import MainLayout from '@layouts/MainLayout';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
+const AskLayout = loadable(() => import('@layouts/AskLayout'));
+const QuestionLayout = loadable(() => import('@layouts/QuestionLayout'));
 const LandingPage = loadable(() => import('@pages/LandingPage'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Login = loadable(() => import('@pages/Login'));
-const MainPage = loadable(() => import('@pages/MainPage'));
+const Error = loadable(() => import('@pages/Error'));
+const AskMain = loadable(() => import('@pages/AskMain'));
+const AskDetail = loadable(() => import('@pages/AskDetail'));
+const AskNew = loadable(() => import('@pages/AskNew'));
+const AIAnswer = loadable(() => import('@pages/AIAnswer'));
+const TempPost = loadable(() => import('@pages/TempPost'));
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<Routes>
 				<Route index element={<LandingPage />} />
-				<Route path="/join" element={<SignUp />} />
+				<Route path="/signup" element={<SignUp />} />
 				<Route path="/login" element={<Login />} />
-				<Route element={<MainLayout />}>
-					<Route path="/main" element={<MainPage />} />
+				<Route path="/ask" element={<AskLayout />}>
+					<Route index element={<AskMain />} />
+					<Route path="ai" element={<AIAnswer />} />
+					<Route path="detail/:id" element={<AskDetail />} />
+					<Route path="new" element={<TempPost />} />
+					<Route path="*" element={<Navigate to="/error" />} />
 				</Route>
+				<Route path="*" element={<Error />} />
 			</Routes>
 		</ThemeProvider>
 	);
